35dfdeaaf4c4d1386c16caa270379113bc6f1c11
mohdshareef88 <mohd.shareef62@gmail.com>
2025-08-05 16:07:45 +0300

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "mydemo-tf-test-bucket"
 
   lifecycle {
     prevent_destroy = false
